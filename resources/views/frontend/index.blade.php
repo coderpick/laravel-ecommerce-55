790cdb0dcae5ddf2d06fe5a09b2a998161815625
Hafizur Rahman <coderpickbd@gmail.com>
2025-06-21 11:38:51 +0600

    feat: Update hero slider images and assets
    Replaces webp slider images with jpgs and adds new hero images. Updates the view to use the new image assets.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,13 +9,13 @@
                     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                                <img src="{{ asset('assets/frontend/images/hero/s1.webp') }}" class="d-block w-100" alt="...">
+                                <img src="{{ asset('assets/frontend/images/hero/slider-img-1.jpg') }}" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
-                                <img src="{{ asset('assets/frontend/images/hero/s2.webp') }}" class="d-block w-100" alt="...">
+                                <img src="{{ asset('assets/frontend/images/hero/slider-img-2.jpg') }}" class="d-block w-100" alt="...">
                             </div>
                             <div class="carousel-item">
-                                <img src="{{ asset('assets/frontend/images/hero/s3.webp') }}" class="d-block w-100" alt="...">
+                                <img src="{{ asset('assets/frontend/images/hero/slider-img-3.jpg') }}" class="d-block w-100" alt="...">
                             </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
